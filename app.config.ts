9b80c000d07db4ac4e9ff98b548d8b0b66388afb
danielbroquet <128781265+danielbroquet@users.noreply.github.com>
2026-04-29 08:36:15 +0200

    Added eas.json

---
+++ app.config.ts
@@ -0,0 +1,68 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "bolt-expo-nativewind",
+  slug: "bolt-expo-nativewind",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "clickk",
+  intentFilters: [
+    {
+      action: "VIEW",
+      autoVerify: true,
+      data: [
+        {
+          scheme: "https",
+          host: "clickk.app",
+          pathPrefix: "/",
+        },
+      ],
+      category: ["BROWSABLE", "DEFAULT"],
+    },
+  ],
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: false,
+    bundleIdentifier: "com.clickk.app",
+    buildNumber: "1",
+    associatedDomains: ["applinks:clickk.app"],
+    infoPlist: {
+      UIBackgroundModes: ["remote-notification"],
+    },
+  },
+  android: {
+    package: "com.clickk.app",
+    versionCode: 1,
+    googleServicesFile: "./google-services.json",
+    permissions: ["INTERNET", "ACCESS_NETWORK_STATE", "VIBRATE"],
+  },
+  web: {
+    bundler: "metro",
+    output: "single",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    ["expo-router", { origin: "https://clickk.app" }],
+    "expo-font",
+    "expo-web-browser",
+    [
+      "expo-notifications",
+      {
+        icon: "./assets/images/notification-icon.png",
+        color: "#00D2B8",
+        defaultChannel: "default",
+      },
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    eas: {
+      projectId: process.env.EAS_PROJECT_ID ?? "YOUR_EAS_PROJECT_ID",
+    },
+  },
+});
